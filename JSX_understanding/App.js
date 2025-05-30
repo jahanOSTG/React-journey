c7a1cd7cbfb16285033e6970dfe09bcbe7928d2b
Faria Jahan Janie <janiefaria315@gmail.com>
2025-05-30 23:39:30 +0600

    Add files via upload

---
+++ JSX_understanding/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+let my="Janie";
+function App() {
+  return (
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+      </nav>
+      <h2>My name is {my}</h2>
+      <div className="container">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores aperiam obcaecati ratione voluptas! In, iure? Accusamus, deserunt sunt magnam possimus illo quidem aliquid quos dicta reiciendis nulla exercitationem doloremque amet.</p>
+      </div>
+      </>
+
+  );
+}
+
+export default App;
